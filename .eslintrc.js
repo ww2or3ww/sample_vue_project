7e6300473f0bf98d7868f4b8f8d0f9da2b3ad7eb
EC2 Default User <ec2-user@ip-172-31-20-246.ap-northeast-1.compute.internal>
2019-12-30 16:33:58 +0000

    refactoring

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,8 @@
 module.exports = { 
-  root: true, 
-  env: { 
-    node: true, 
-  }, 
   extends: [ 
     'plugin:vue/essential', 
   ], 
   rules: { 
     'no-console': 'off', 
-  }, 
-  parserOptions: { 
-    parser: 'babel-eslint', 
   }, 
 };
